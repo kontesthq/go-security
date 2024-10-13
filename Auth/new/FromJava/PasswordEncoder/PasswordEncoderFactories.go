aa13aaae1ecaa4bad4217db371d0ac354b2401b3
Ayush Singhal <ayushsinghals02@gmail.com>
2024-10-13 19:25:53 +0530

    Added PasswordEncoderFactories.go

---
+++ Auth/new/FromJava/PasswordEncoder/PasswordEncoderFactories.go
@@ -0,0 +1,22 @@
+package PasswordEncoder
+
+type PasswordEncoderFactories struct {
+}
+
+// CreateDelegatingPasswordEncoder creates a DelegatingPasswordEncoder with default mappings.
+// Additional mappings may be added and the encoding will be updated to conform with best practices.
+// The current mappings are:
+// - bcrypt: BCryptPasswordEncoder
+// - noop: NoOpPasswordEncoder
+// - scrypt: SCryptPasswordEncoder
+func (p *PasswordEncoderFactories) CreateDelegatingPasswordEncoder() (*DelegatingPasswordEncoder, error) {
+	encodingId := "bcrypt"
+	encoders := map[string]PasswordEncoder{
+		encodingId: new(BCryptPasswordEncoder),
+		"noop":     new(NoOpPasswordEncoder),
+		"scrypt":   new(SCryptPasswordEncoder),
+		// Add other encoders as needed.
+	}
+
+	return NewDelegatingPasswordEncoder(encodingId, encoders)
+}
